3ab2cf207b4961012075e521afee0695e1f597da
primatara42 <primatara42@gmail.com>
2022-10-02 22:50:50 +0700

    Update PrimaPutraClass.java

--- src/githubpackage/PrimaPutraClass.java
+++ src/githubpackage/PrimaPutraClass.java
@@ -2,7 +2,9 @@ package githubpackage;
 
 public class PrimaPutraClass
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+        System.out.println("HALO SEMUAAAAA!");
     }
 }
